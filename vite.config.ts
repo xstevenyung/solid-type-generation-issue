9d33ab1ba7f8927c1dc6da1c13cf1f7ea8eba268
Steven Yung <stevenyung@fastmail.com>
2021-07-30 09:47:08 +0200

    add setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,35 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import { resolve } from "path";
+import pkg from "./package.json";
 
 export default defineConfig({
   plugins: [solidPlugin()],
   build: {
-    target: "esnext",
-    polyfillDynamicImport: false,
+    lib: {
+      name: pkg.name,
+      fileName: "index",
+      entry: resolve(__dirname, "src/index.ts"),
+    },
+
+    rollupOptions: {
+      external: [
+        "solid-js",
+        "solid-js/web",
+        "solid-styled-components",
+        "solid-js/store",
+      ],
+
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          "solid-js": "Solid",
+          "solid-js/web": "SolidWeb",
+          "solid-styled-components": "SolidStyledComponent",
+          "solid-js/store": "SolidStore",
+        },
+      },
+    },
   },
 });
